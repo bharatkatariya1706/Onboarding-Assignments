1ed34b37ab78c5ef4bd4de8471d0c5ce7500cd61
Bharat Katariya <bharatrajkatariya24@gmail.com>
2025-07-21 21:04:30 +0530

    added user input feature and tests for it using vitest

---
+++ Assignment-01/src/utils/input.ts
@@ -0,0 +1,8 @@
+export async function getUserInput(prompt: string): Promise<string> {
+  return new Promise((resolve) => {
+    console.log(prompt);
+    process.stdin.once("data", (data) => {
+      resolve(data.toString().trim());
+    });
+  });
+}
